647deb1ab0f2cc40b45f611a960e3b7033cde82f
Paranhos-Wendell <128772901+Paranhos-Wendell@users.noreply.github.com>
2024-03-01 22:59:58 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,60 @@
+
+const seatsContainer = document.querySelector('.seats');
+const selectedSeatsList = document.querySelector('.selected-seats');
+const totalElement = document.getElementById('total');
+let total = 0;
+
+
+// Cria as cadeiras
+for (let i = 1; i <= 44; i++) {
+
+    const seat = document.createElement('div');
+    seat.classList.add('seat');
+
+    seat.innerText = i;
+
+    // Adiciona evento de clique para selecionar a cadeira
+    seat.addEventListener('click', () => {
+        seat.classList.toggle('selected');
+        updateSummary();
+    });
+
+    seatsContainer.appendChild(seat);
+}
+
+// Atualiza o resumo da seleção
+function updateSummary() {
+    const selectedSeats = document.querySelectorAll('.seat.selected');
+    selectedSeatsList.innerHTML = '';
+    total = 0;
+
+    selectedSeats.forEach(seat => {
+        const seatNumber = seat.innerText;
+        const listItem = document.createElement('li');
+        listItem.innerText = `Cadeira Nº ${seatNumber} `;
+        selectedSeatsList.appendChild(listItem);
+
+        // Incrementa o total com o preço da cadeira selecionada
+        total += 10;
+    });
+
+    totalElement.innerText = total;
+
+    seatNumber.innerHTML = n;
+}
+
+// Confirma a seleção
+function confirm() {
+    const selectedSeats = document.querySelectorAll('.seat.selected');
+    const totalSeats = selectedSeats.length;
+    alert(`Compra realizada!\nTotal: R$ ${total} você comprou um total de ${totalSeats} cadeiras`);
+}
+
+// Limpa a seleção de cadeiras
+function clearSelection() {
+    const selectedSeats = document.querySelectorAll('.seat.selected');
+    selectedSeats.forEach(seat => {
+        seat.classList.remove('selected');
+    });
+    updateSummary();
+}
